f21bc727715735840b15d5df746270bd7a64b7a7
David Pearl <davidtpearl@gmail.com>
2019-10-01 22:27:04 +1000

    Typescript example function

---
+++ 100Algorithims/example.ts
@@ -0,0 +1,6 @@
+function add(num1: number, num2: number): number {
+    const example: number[] = [0, 1, 2]; // expects number data type
+    const example2: any[] = ["a string", 1, true]; // can be any data type
+
+    return 1;
+}
