df983e85a0fb2b54639982010e3a66b362ee69a0
David Pearl <davidtpearl@gmail.com>
2019-10-03 12:49:11 +1000

    Algorithm 3 - add border

--- 100Algorithims/example.ts
+++ 100Algorithims/example.ts
@@ -1,6 +1,10 @@
-function add(num1: number, num2: number): number {
+const example: number[] = [1, 2, 3, 4];
+const example2: (number | boolean)[] = [1, 2, 3, true];
+const example3: any[] = [1, 2, "three", false];
+
+function add(num1: number, num2: number): number { // require returned value of function to have type: number
     const example: number[] = [0, 1, 2]; // expects number data type
     const example2: any[] = ["a string", 1, true]; // can be any data type
 
-    return 1;
+    return num1 + num2; // return value is type: number
 }
